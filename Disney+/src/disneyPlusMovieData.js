a216835295603eb320c985a2c39d3fa128dc4f2d
professionaltechs <146634572+muzzammil-123@users.noreply.github.com>
2024-07-19 20:06:54 +0500

    Update disneyPlusMovieData.js

--- Disney+/src/disneyPlusMovieData.js
+++ Disney+/src/disneyPlusMovieData.js
@@ -1,5 +1,5 @@
 let data = [
-
+    
     {
         "id" : "GPT3POINT5ROCKS2024AMAZINGUNIQUEID1",
         "backgroundImg": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg",
